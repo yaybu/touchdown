cc1f56230f37eab1c2079154184df5659c2325ae
John Carr <john.carr@isotoma.com>
2014-09-10 15:12:24 +0100

    Skeleton package

---
+++ README.rst
@@ -0,0 +1,17 @@
+=========
+Touchdown
+=========
+
+.. image:: https://travis-ci.org/yaybu/touchdown.png?branch=master
+   :target: https://travis-ci.org/#!/yaybu/touchdown
+
+.. image:: https://coveralls.io/repos/yaybu/touchdown/badge.png?branch=master
+    :target: https://coveralls.io/r/yaybu/touchdown
+
+.. image:: https://pypip.in/version/touchdown/badge.png
+    :target: https://pypi.python.org/pypi/touchdown/
+
+
+Touchdown is a service orchestration framework for python.
+
+You can find us in #yaybu on irc.oftc.net.
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1.dev0'
+
+setup(
+    name='touchdown',
+    version=version,
+    url="http://yaybu.com/",
+    description="Orchestration and configuration management in Python",
+    long_description = open("README.rst").read(),
+    author="Isotoma Limited",
+    author_email="support@isotoma.com",
+    license="Apache Software License",
+    classifiers = [
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'six',
+    ],
+    extras_require = {
+        'test': ['unittest2', 'mock'],
+        },
+)
