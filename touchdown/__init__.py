49d4221786764544df5c0ba640e80c45eef5db07
John Carr <john.carr@unrouted.co.uk>
2015-01-18 16:45:41 +0000

    Remove stray logging config

--- touchdown/__init__.py
+++ touchdown/__init__.py
@@ -12,8 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
 import touchdown.core  # noqa
 import touchdown.aws  # noqa
