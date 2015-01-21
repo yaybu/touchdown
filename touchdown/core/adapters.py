184d8e88017726e695ee9cb22e428b667f6d22de
John Carr <john.carr@unrouted.co.uk>
2015-01-21 14:37:29 +0000

    Missing files

---
+++ touchdown/core/adapters.py
@@ -0,0 +1,36 @@
+# Copyright 2015 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from touchdown.core import argument, errors
+from touchdown.core.resource import Resource
+
+
+class Adapter(Resource):
+
+    adapts = argument.Resource(Resource)
+    input = None
+
+    @property
+    def serializer(self):
+        try:
+            return self.adapt()
+        except NotImplementedError:
+            return super(Adapter, self).serializer
+
+    @classmethod
+    def adapt(cls, resource):
+        for adapter in cls.__subclasses__():
+            if adapter.input and isinstance(resource, adapter.input):
+                return adapter(resource, adapts=resource)
+        raise errors.Error("Cannot turn {} into a {}".format(resource, cls))
