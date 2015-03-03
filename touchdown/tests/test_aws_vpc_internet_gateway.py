7c2ca860f7d293b019a9b45359d1845cd4a593ab
John Carr <john.carr@unrouted.co.uk>
2015-03-03 20:56:07 +0000

    Add test for internet gateway

---
+++ touchdown/tests/test_aws_vpc_internet_gateway.py
@@ -0,0 +1,29 @@
+# Copyright 2015 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from . import aws
+
+
+class TestInternetGateway(aws.RecordedBotoCoreTest):
+
+    def test_create_and_delete_igw(self):
+        vpc = self.aws.add_vpc(
+            name='test-vpc',
+            cidr_block='192.168.0.0/25',
+        )
+        vpc.add_internet_gateway(
+            name='test-igw',
+        )
+        self.apply()
+        self.destroy()
