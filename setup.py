94fc88d1b416c885cff1c19f6d80570b8ab5a90d
John Carr <john.carr@unrouted.co.uk>
2016-02-09 22:03:08 +0000

    Track botocore release

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'contextlib2',
         'netaddr',
         'fuselage>=0.0.10',
-        'botocore>=1.3.23',
+        'botocore>=1.3.24',
         'progressbar2>=3.6.0',
         'requests',
         'python-gnupg',
