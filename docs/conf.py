5b07237c237698dfdd91e10fc639002858910e07
John Carr <john.carr@unrouted.co.uk>
2016-03-15 06:53:15 +0000

    Flake8 changes

--- docs/conf.py
+++ docs/conf.py
@@ -11,9 +11,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import os
-import sys
-
 import pkg_resources
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -176,10 +173,13 @@ htmlhelp_basename = 'touchdowndoc'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-  ('index', 'touchdown.tex', u'touchdown documentation',
-   u'John Carr', 'manual'),
-]
+latex_documents = [(
+    'index',
+    'touchdown.tex',
+    u'touchdown documentation',
+    u'John Carr',
+    'manual'
+)]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import os
-
 from setuptools import find_packages, setup
 
 version = '0.7.5.dev0'
@@ -9,11 +7,11 @@ setup(
     version=version,
     url="http://yaybu.com/",
     description="Orchestration and configuration management in Python",
-    long_description = open("README.rst").read(),
+    long_description=open("README.rst").read(),
     author="Isotoma Limited",
     author_email="support@isotoma.com",
     license="Apache Software License",
-    classifiers = [
+    classifiers=[
         "Intended Audience :: System Administrators",
         "Operating System :: POSIX",
         "License :: OSI Approved :: Apache Software License",
