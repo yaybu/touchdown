710114a98bebfe71f62414c7d7aa95aa373798e7
John Carr <john.carr@unrouted.co.uk>
2016-04-29 14:35:56 +0100

    Missing '.'

--- touchdown/aws/cloudfront/distribution.py
+++ touchdown/aws/cloudfront/distribution.py
@@ -28,7 +28,7 @@ from ..common import (
     SimpleDestroy,
 )
 from ..iam import ServerCertificate
-from .elb import LoadBalancer
+from ..elb import LoadBalancer
 from ..s3 import Bucket
 from .common import CloudFrontList, CloudFrontResourceList, S3Origin
 
