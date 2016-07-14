2bc46963f1aad773933d204891d98eee0ec336da
John Carr <john.carr@isotoma.com>
2016-07-14 15:45:40 +0000

    AWS WAF: Fix updating existing resources

--- touchdown/aws/waf/rule.py
+++ touchdown/aws/waf/rule.py
@@ -62,6 +62,7 @@ class Rule(Resource):
         Match,
         field='Predicates',
         create=False,
+        serializer=serializers.List(serializers.Resource()),
     )
     account = argument.Resource(BaseAccount)
 
