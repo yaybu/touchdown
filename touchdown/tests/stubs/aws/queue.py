452f6336ae4d6f7a752bfe16298760889123c7d2
John Carr <john.carr@unrouted.co.uk>
2016-08-27 14:06:06 +0100

    Tests: Move SQS to botocore stub tests

---
+++ touchdown/tests/stubs/aws/queue.py
@@ -0,0 +1,71 @@
+# Copyright 2016 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .service import ServiceStubber
+
+
+class QueueStubber(ServiceStubber):
+
+    client_service = 'sns'
+
+    def add_get_queue_url(self):
+        return self.add_response(
+            'get_queue_url',
+            service_response={
+                'QueueUrl': 'https://' + self.resource.name,
+            },
+            expected_params={
+                'QueueName': self.resource.name,
+            },
+        )
+
+    def add_get_queue_url_404(self):
+        return self.add_client_error(
+            'get_queue_url',
+            service_error_code='AWS.SimpleQueueService.NonExistentQueue',
+            service_message='',
+        )
+
+    def add_get_queue_attributes(self):
+        return self.add_response(
+            'get_queue_attributes',
+            service_response={
+                'Attributes': {},
+            },
+            expected_params={
+                'AttributeNames': ['All'],
+                'QueueUrl': 'https://' + self.resource.name,
+            },
+        )
+
+    def add_create_queue(self):
+        return self.add_response(
+            'create_queue',
+            service_response={
+                'QueueUrl': 'https://' + self.resource.name,
+            },
+            expected_params={
+                'QueueName': self.resource.name,
+            }
+        )
+
+    def add_delete_queue(self):
+        return self.add_response(
+            'delete_queue',
+            service_response={
+            },
+            expected_params={
+                'QueueUrl': 'https://' + self.resource.name,
+            }
+        )
--- touchdown/tests/test_aws_sqs_queue.py
+++ touchdown/tests/test_aws_sqs_queue.py
@@ -12,14 +12,87 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import aws
+from touchdown.tests.aws import StubberTestCase
+from touchdown.tests.stubs.aws import QueueStubber
 
 
-class TestQueue(aws.RecordedBotoCoreTest):
+class TestQueueCreation(StubberTestCase):
 
-    def test_create_and_delete_queue(self):
-        self.aws.add_queue(
-            name='test-queue',
-        )
-        self.apply()
-        self.destroy()
+    def test_create_queue(self):
+        goal = self.create_goal('apply')
+
+        queue = self.fixtures.enter_context(QueueStubber(
+            goal.get_service(
+                self.aws.add_queue(
+                    name='test-queue',
+                ),
+                'apply',
+            )
+        ))
+
+        queue.add_get_queue_url_404()
+        queue.add_create_queue()
+
+        queue.add_get_queue_url()
+        queue.add_get_queue_attributes()
+
+        queue.add_get_queue_url()
+        queue.add_get_queue_attributes()
+
+        goal.execute()
+
+    def test_create_queue_idempotent(self):
+        goal = self.create_goal('apply')
+
+        queue = self.fixtures.enter_context(QueueStubber(
+            goal.get_service(
+                self.aws.add_queue(
+                    name='test-queue',
+                ),
+                'apply',
+            )
+        ))
+
+        queue.add_get_queue_url()
+        queue.add_get_queue_attributes()
+
+        self.assertEqual(len(list(goal.plan())), 0)
+        self.assertEqual(len(goal.get_changes(queue.resource)), 0)
+
+
+class TestQueueDestroy(StubberTestCase):
+
+    def test_destroy_queue(self):
+        goal = self.create_goal('destroy')
+
+        queue = self.fixtures.enter_context(QueueStubber(
+            goal.get_service(
+                self.aws.add_queue(
+                    name='test-queue',
+                ),
+                'destroy',
+            )
+        ))
+
+        queue.add_get_queue_url()
+        queue.add_get_queue_attributes()
+        queue.add_delete_queue()
+
+        goal.execute()
+
+    def test_destroy_queue_idempotent(self):
+        goal = self.create_goal('destroy')
+
+        queue = self.fixtures.enter_context(QueueStubber(
+            goal.get_service(
+                self.aws.add_queue(
+                    name='test-queue',
+                ),
+                'destroy',
+            )
+        ))
+
+        queue.add_get_queue_url_404()
+
+        self.assertEqual(len(list(goal.plan())), 0)
+        self.assertEqual(len(goal.get_changes(queue.resource)), 0)
